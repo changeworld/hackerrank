ebc62d3888473cedd4ac8ff3c40d66dc90a27240
changeworld <changesworlds@gmail.com>
2016-02-03 00:45:53 +0900

    Ruby - Strings - Introduction

---
+++ ruby-strings-introduction.rb
@@ -0,0 +1,16 @@
+def single_quote
+  # single quote string here
+  'Hello World and others!'
+end
+
+def double_quote
+  # Double quote string here
+  "Hello World and others!"
+end
+
+def here_doc
+  # Here doc string here
+  <<-EOM
+  Hello World and others!
+  EOM
+end
