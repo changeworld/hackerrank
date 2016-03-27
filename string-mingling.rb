12d801d116f2f62e2eae2c29ad936b443145950b
changeworld <changesworlds@gmail.com>
2016-03-28 08:11:13 +0900

    String Mingling

---
+++ string-mingling.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+object Solution {
+  def main(args: Array[String]) {
+    val a = readLine()
+    val b = readLine()
+    for(i<-0 until a.length) {
+      print(a(i))
+      print(b(i))
+    }
+  }
+}
