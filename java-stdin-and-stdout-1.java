583a05549f54aaa23dff8b99b09b17b4a96c7db7
changeworld <changesworlds@gmail.com>
2016-04-07 07:10:08 +0900

    Fix

--- java-stdin-and-stdout-1.java
+++ java-stdin-and-stdout-1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    for(int i = 0; i<3; i++){
+    for(int i=0; i<3; i++){
       int a = sc.nextInt();
       System.out.println(a);
     }
