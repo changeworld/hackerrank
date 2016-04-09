5796e1057c3900ef2b606ad2e167c455acdc457a
changeworld <changesworlds@gmail.com>
2016-04-10 03:11:45 +0900

    Java End-of-file

---
+++ java-end-of-file.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    for(int i=1; sc.hasNext(); i++){
+      System.out.println(String.valueOf(i) + " " + sc.nextLine());
+    }
+  }
+}
