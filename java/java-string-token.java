54de9088014c076e4a77d103627f09e01be1982d
changeworld <changesworlds@gmail.com>
2016-04-16 06:13:28 +0900

    Java String Token

---
+++ java/java-string-token.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String[] text = sc.nextLine().trim().split("[ !,?\\._'@]+",0);
+    if(text.length == 1 && text[0].equals("")){
+      System.out.println(0);
+    }else{
+      System.out.println(text.length);
+      for(String s:text) System.out.println(s);
+    }
+  }
+}
