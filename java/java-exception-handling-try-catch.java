629b25920f536e99c6adc825ac1976a0ea88cab1
changeworld <changesworlds@gmail.com>
2016-04-24 06:01:27 +0900

    Java Exception Handling (Try-catch)

---
+++ java/java-exception-handling-try-catch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Solution{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    try{
+      int a = sc.nextInt(),b = sc.nextInt();
+      System.out.println(a/b);
+    }catch(InputMismatchException e){
+      System.out.println(e.getClass().toString().substring(6));
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
