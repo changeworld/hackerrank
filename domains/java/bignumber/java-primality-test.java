55e8029eef19ebf25d5721ae4003b967cea44c96
changeworld <changesworlds@gmail.com>
2016-06-12 21:24:41 +0900

    Java Primality Test

---
+++ domains/java/bignumber/java-primality-test.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Solution{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    BigInteger n = sc.nextBigInteger();
+    sc.close();
+    System.out.println(n.isProbablePrime(100) ? "prime" : "not prime");
+  }
+}
