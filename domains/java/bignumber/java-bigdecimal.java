7a72286bbd8333cde853ec83ba6ef56b28ddb79a
changeworld <changesworlds@gmail.com>
2016-06-12 21:32:54 +0900

    Modify import packages

--- domains/java/bignumber/java-bigdecimal.java
+++ domains/java/bignumber/java-bigdecimal.java
@@ -1,7 +1,9 @@
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
-class Solution {
+class Solution{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
