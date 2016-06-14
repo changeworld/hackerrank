5f67000250b006e950cc446de08cd489cdd45151
changeworld <changesworlds@gmail.com>
2016-06-14 23:46:48 +0900

    Modify position of the Java Advanced

--- domains/java/java-advanced/java-reflection-attributes.java
+++ domains/java/java-advanced/java-reflection-attributes.java
@@ -2,7 +2,6 @@ public class Solution{
   public static void main(String[] args){
     Class student = Student.class;
     Method[] methods = student.getDeclaredMethods();
-
     ArrayList<String> methodList = new ArrayList<>();
     for(Method method: methods){
       methodList.add(method.getName());
