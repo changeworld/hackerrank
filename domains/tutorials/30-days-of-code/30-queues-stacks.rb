ee493ff64ed97dcd188f2570ec403f9bcf42cd8b
Takashi Takebayashi <changesworlds@gmail.com>
2016-07-19 19:15:36 +0900

    Day 18: Queues and Stacks

---
+++ domains/tutorials/30-days-of-code/30-queues-stacks.rb
@@ -0,0 +1,39 @@
+# create the Solution class
+class Solution < Array
+  # complete the code
+  alias :push_character :push
+  def enqueue_character(x)
+  end
+  alias :pop_character :pop
+  alias :dequeue_character :shift
+end
+
+# create Solution class object
+solution = Solution.new
+
+# read the input
+input = gets
+
+input.split('').each do |c|
+  # push the character to stack
+  solution.push_character c
+  # enqueue the character to queue
+  solution.enqueue_character c
+end
+
+# check if input string is palindrome or not
+is_palindrome = true
+
+(input.length / 2).times do
+  if solution.pop_character != solution.dequeue_character
+    is_palindrome = false
+    break
+  end
+end
+
+# print if string is palindrome or not
+if is_palindrome
+  puts "The word, #{input}, is a palindrome."
+else
+  puts "The word, #{input}, is not a palindrome."
+end
