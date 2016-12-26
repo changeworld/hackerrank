0a3c9712e5eb7f63e3a3f1ef11e0c2a239aed019
Takashi Takebayashi <changesworlds@gmail.com>
2016-12-27 00:32:25 +0900

    Java Map

---
+++ domains/java/java-data-structure/phone-book.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        for (; 0 < N; N--) {
+            map.put(sc.nextLine(), sc.nextLine());
+        }
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            if (map.containsKey(s)) System.out.println(s + "=" + map.get(s));
+            else System.out.println("Not found");
+        }
+    }
+}
