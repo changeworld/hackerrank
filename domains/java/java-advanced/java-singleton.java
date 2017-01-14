d7ebb24a76bd67f5308fa0dabb1cb2107f9c2e1b
Takashi Takebayashi <changesworlds@gmail.com>
2017-01-15 00:05:32 +0900

    Java Singleton Pattern

---
+++ domains/java/java-advanced/java-singleton.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Constructor;
+import java.util.Scanner;
+
+class Singleton {
+    private static Singleton singleton = new Singleton();
+    public String str;
+
+    private Singleton() {
+    }
+
+    public static Singleton getSingleInstance() {
+        return singleton;
+    }
+}
