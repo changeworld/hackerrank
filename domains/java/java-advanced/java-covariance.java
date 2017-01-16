5ee98ee45ac1ef02cf4c4ce358083ca94bdfa2df
Takashi Takebayashi <changesworlds@gmail.com>
2017-01-17 00:04:32 +0900

    Covariant Return Types

---
+++ domains/java/java-advanced/java-covariance.java
@@ -0,0 +1,47 @@
+class Flower {
+    String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+
+class Jasmine extends Flower {
+    String whatsYourName() {
+        return "Jasmine";
+    }
+}
+
+class Lily extends Flower {
+    String whatsYourName() {
+        return "Lily";
+    }
+}
+
+class Lotus extends Flower {
+    String whatsYourName() {
+        return "Lotus";
+    }
+}
+
+class State {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+
+class WestBengal extends State {
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+
+class Karnataka extends State {
+    Lotus yourNationalFlower() {
+        return new Lotus();
+    }
+}
+
+class AndhraPradesh extends State {
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
