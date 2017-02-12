16885fd7efabe815bfeed88ab24eaf2fd0ada961
Takashi Takebayashi <changesworlds@gmail.com>
2017-02-12 15:57:12 +0900

    No Idea!

---
+++ domains/python/py-sets/no-idea.py
@@ -0,0 +1,8 @@
+input()
+X = [int(e) for e in input().split()]
+A = {int(e) for e in input().split()}
+B = {int(e) for e in input().split()}
+r = 0
+for e in X:
+    r += (e in A)-(e in B)
+print(r)
