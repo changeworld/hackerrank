91a244b453a3e8570328b541c864d57723ebda9f
Takashi Takebayashi <changesworlds@gmail.com>
2017-02-18 00:43:29 +0900

    DefaultDict Tutorial

---
+++ domains/python/py-collections/defaultdict-tutorial.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+N, M = map(int, input().split())
+d = defaultdict(list)
+for i in range(N):
+    s = input()
+    d[s].append(i+1)
+for _ in range(M):
+    s = input()
+    if s in d:
+        print(' '.join(map(str,d[s])))
+    else:
+        print('-1')
