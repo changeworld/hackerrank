3283daafc521feb7ef1b25feeb8a7f1d961d020c
Takashi Takebayashi <changesworlds@gmail.com>
2017-02-26 01:02:35 +0900

    Collections.OrderedDict()

---
+++ domains/python/py-collections/py-collections-ordereddict.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict
+
+d = OrderedDict()
+for _ in range(int(input())):
+    n = input().split()
+    p = int(n.pop())
+    n = ' '.join(n)
+    if n not in d:
+        d[n] = 0
+    d[n] += p
+for n in d:
+	print(n+' '+str(d[n]))
