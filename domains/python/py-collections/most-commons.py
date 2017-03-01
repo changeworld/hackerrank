b73c153574793e05f556d3f86ae0d23b577d8ea4
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-02 00:27:49 +0900

    Most Common

---
+++ domains/python/py-collections/most-commons.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+S = Counter(input().rstrip())
+i = 0
+for v, k in sorted((-v, k) for k, v in S.most_common()):
+    print(k+' '+str(-v))
+    i += 1
+    if i >=3:
+        break
