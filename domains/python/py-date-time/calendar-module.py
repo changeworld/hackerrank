750cad85f5735d4ad3cc09936350ce78f769c099
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-03 00:23:19 +0900

    Calendar Module

---
+++ domains/python/py-date-time/calendar-module.py
@@ -0,0 +1,4 @@
+from datetime import date
+
+MM, DD, YYYY = map(int, input().split())
+print(['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'][date(YYYY, MM, DD).weekday()])
