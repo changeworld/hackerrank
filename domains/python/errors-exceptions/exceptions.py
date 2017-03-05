9b5c9c4cb7769c3e9411981331c1724bcf21c7db
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-06 00:34:22 +0900

    Exceptions

---
+++ domains/python/errors-exceptions/exceptions.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    try:
+        x, y = map(int, input().split())
+        print(x//y)
+    except ValueError as e:
+        print("Error Code: "+str(e))
+    except ZeroDivisionError as e:
+        print("Error Code: "+str(e))
