13f3a549801c90b1da3c045b05b134d802a4c6b8
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-22 00:46:31 +0900

    HTML Parser - Part 1

---
+++ domains/python/py-regex/html-parser-part-1.py
@@ -0,0 +1,15 @@
+from html.parser import HTMLParser
+
+class CustumHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('Start : ' + tag)
+        for e in attrs: print("-> " + e[0] + " > " + str(e[1]))
+    def handle_endtag(self, tag):
+        print('End   : ' + tag)
+    def handle_startendtag(self, tag, attrs):
+        print('Empty : ' + tag)
+        for e in attrs: print("-> " + e[0] + " > " + str(e[1]))
+
+parser = CustumHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
