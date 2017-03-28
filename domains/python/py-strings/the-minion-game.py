d3594eb1bba18fda77e58c137b05ebde8bf212e3
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-29 00:59:21 +0900

    The Minion Game

---
+++ domains/python/py-strings/the-minion-game.py
@@ -0,0 +1,17 @@
+def minion_game(s):
+    n = len(s)
+    r = [0, 0]
+    for i in range(n):
+        if s[i] in ('AEIOU'):
+            r[0] += n - i
+        else:
+            r[1] += n - i
+    if r[0] > r[1]:
+        print('Kevin', r[0])
+    elif r[1] > r[0]:
+        print('Stuart', r[1])
+    else:
+        print('Draw')
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
