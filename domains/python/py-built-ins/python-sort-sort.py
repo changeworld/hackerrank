ce3881db754d37927bb58d89e5cc542e4d4187b0
Takashi Takebayashi <changesworlds@gmail.com>
2017-03-31 01:05:32 +0900

    Sort Data

---
+++ domains/python/py-built-ins/python-sort-sort.py
@@ -0,0 +1,5 @@
+N, M = map(int, input().split())
+a = [[int(e) for e in input().split()] for _ in range(N)]
+K = int(input())
+for e in sorted(a, key=lambda e:e[K]):
+    print(' '.join(map(str, e)))
