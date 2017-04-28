c52b4a50e682642698a6099277b9f3243f545231
Takashi Takebayashi <changesworlds@gmail.com>
2017-04-29 02:18:16 +0900

    Validating Email Addresses With a Filter

---
+++ domains/python/py-functionals/validate-list-of-email-address-with-filter.py
@@ -0,0 +1,16 @@
+import re
+def fun(s):
+    return re.match(r'^[0-9a-zA-Z\-_]+@[0-9a-zA-Z]+\.[a-zA-Z]{1, 3}$', s)
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
