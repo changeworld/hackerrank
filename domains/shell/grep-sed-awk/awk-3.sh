5f8283007047a02731a4f6f413612089d02092b0
Takashi Takebayashi <changesworlds@gmail.com>
2017-07-30 00:47:32 +0900

    'Awk' - 3

---
+++ domains/shell/grep-sed-awk/awk-3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+awk '{
+s=0;
+for(i=2; i <= NF ; i++) s+=$i
+avg=s/(NF-1);
+if (avg < 50) grade = "FAIL"
+else if (avg >= 60 && avg <= 80) grade = "B"
+else if (avg >= 80) grade = "A"
+print ($0" : "grade) }' $1
