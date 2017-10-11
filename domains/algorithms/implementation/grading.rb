5f5f9cb9eec905616fb36a42ea48fd71c2bb438b
Takashi Takebayashi <changesworlds@gmail.com>
2017-10-12 00:38:55 +0900

    Grading Students

---
+++ domains/algorithms/implementation/grading.rb
@@ -0,0 +1,17 @@
+#!/bin/ruby
+
+def solve(grades)
+  result = []
+  grades.map{|grade|
+    result.push((grade >= 38 && grade % 5 >= 3) ? grade + 5 - (grade % 5) : grade)
+  }
+  result
+end
+
+n = gets.strip.to_i
+grades = Array.new(n)
+for grades_i in (0..n-1)
+  grades[grades_i] = gets.strip.to_i
+end
+result = solve(grades)
+print result.join("\n")
