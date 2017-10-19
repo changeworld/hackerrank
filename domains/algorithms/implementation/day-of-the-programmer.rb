a2208512b50a0147bdbc153b16aa45af62219678
Takashi Takebayashi <changesworlds@gmail.com>
2017-10-20 00:34:51 +0900

    Day of the Programmer

---
+++ domains/algorithms/implementation/day-of-the-programmer.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+require 'date'
+
+def solve(year)
+  dd = 13
+  dd = 12 if ((year <= 1917) && (year % 4 == 0)) || Date.new(year).leap?
+  dd = 26 if (year == 1918)
+  dd.to_s + ".09." + year.to_s
+end
+
+year = gets.strip.to_i
+result = solve(year)
+puts result;
