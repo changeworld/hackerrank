26d9598ed395c9d23817193f7240b9a1d9a18927
Takashi Takebayashi <changesworlds@gmail.com>
2017-10-29 00:58:17 +0900

    Climbing the Leaderboard

---
+++ domains/algorithms/implementation/climbing-the-leaderboard.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+from bisect import bisect_right
+
+n = int(input().strip())
+scores = sorted(set(map(int,input().split())))
+m = int(input().strip())
+alice = map(int,input().split())
+for i in alice:
+    print(len(scores)-bisect_right(scores,i)+1)
