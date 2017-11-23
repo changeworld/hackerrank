29454ea4d245fc14707999d1e84aec6b722838c0
Takashi Takebayashi <changesworlds@gmail.com>
2017-11-23 13:16:34 +0900

    Fix Reek warnings
    - Fix UncommunicativeVariableName
    - Fix UtilityFunction

--- domains/ruby/ruby-strings/ruby-strings-methods-ii.rb
+++ domains/ruby/ruby-strings/ruby-strings-methods-ii.rb
@@ -1,9 +1,9 @@
-# Enter your code here
+# :reek:UtilityFunction:public_methods_only: true
 def strike(str)
   '<strike>'+str+'</strike>'
 end
 
 def mask_article(str, arr)
-  arr.each{|e| str.gsub!(e, &method(:strike))}
+  arr.each{|elm| str.gsub!(elm, &method(:strike))}
   str
 end
