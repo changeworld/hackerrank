e1ff26b5b9abb6c04ecc44b8c485998bf7bde58d
Takashi Takebayashi <changesworlds@gmail.com>
2017-11-27 15:41:44 +0900

    Fix Reek warnings

--- domains/algorithms/strings/separate-the-numbers.rb
+++ domains/algorithms/strings/separate-the-numbers.rb
@@ -1,31 +1,25 @@
 #!/bin/ruby
 
-def beautiful_string(s)
-  flag = false
-  for i in 1..(s.size/2)+1
-    t = s[0, i].to_i
-    if s == generate_beautiful_string(t, s.size)
-    puts "YES #{t}"
-    flag = true
-    return
-    end
+# :reek:DuplicateMethodCall { max_calls: 2 }
+# :reek:FeatureEnvy:exclude
+def beautiful_string(str)
+  for idx in 1..(str.size/2)+1
+    tmp = str[0, idx].to_i
+    return tmp if str == generate_beautiful_string(tmp, str.size)
   end
-  puts 'NO' if flag == false
+  return false
 end
 
-def generate_beautiful_string(s, length)
-  rtn = ''
-  idx = 0
+# :reek:UtilityFunction:public_methods_only: true
+def generate_beautiful_string(str, length)
+  rtn, idx = '', 0
   while rtn.size < length
-    rtn += s.to_s
-    s += 1
-    idx += 1
+    rtn, str, idx = rtn + str.to_s, str + 1, idx + 1
   end
   return idx == 1 ? '' : rtn
 end
 
-q = gets.strip.to_i
-for a0 in (0..q-1)
-  s = gets.strip
-  beautiful_string(s)
-end
+gets.strip.to_i.times{
+  rtn = beautiful_string(gets.strip)
+  puts rtn ? "YES #{rtn}" : 'NO'
+}
