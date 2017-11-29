fc2d112d86928ebb89f8d1bcc4a231f06c5d9666
Takashi Takebayashi <changesworlds@gmail.com>
2017-11-29 09:21:56 +0900

    Fix Reek warnings

--- domains/algorithms/arrays-and-sorting/insertionsort2.rb
+++ domains/algorithms/arrays-and-sorting/insertionsort2.rb
@@ -1,12 +1,16 @@
 #!/bin/ruby
 
+# :reek:DuplicateMethodCall { max_calls: 2 }
+# :reek:FeatureEnvy:exclude
+# :reek:NestedIterators { max_allowed_nesting: 2 }
+# :reek:TooManyStatements { max_statements: 7 }
 def insertion_sort(arr)
-  (1..(arr.length-1)).each do |i|
-    (i-1).downto(0) do |j|
-      if arr[j] > arr[i]
-        arr[j], arr[i] = arr[i], arr[j]
-        i = j
-        puts arr.join(' ') if i == 0
+  (1..(arr.length-1)).each do |idx|
+    (idx-1).downto(0) do |idex|
+      if arr[idex] > arr[idx]
+        arr[idex], arr[idx] = arr[idx], arr[idex]
+        idx = idex
+        puts arr.join(' ') if idx == 0
       else
         puts arr.join(' ')
         break
@@ -16,5 +20,4 @@ def insertion_sort(arr)
 end
 
 _ = gets.to_i
-arr = gets.strip.split.map(&:to_i)
-insertion_sort(arr)
+insertion_sort(gets.strip.split.map(&:to_i))
