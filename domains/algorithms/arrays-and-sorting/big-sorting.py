7431404b056f20eec4d1ec625a4d8e2562660fcf
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-01 00:24:32 +0900

    Big Sorting

---
+++ domains/algorithms/arrays-and-sorting/big-sorting.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+import sys
+
+unsorted = []
+unsorted_i = 0
+for unsorted_i in range(int(input().strip())):
+    unsorted_t = str(input().strip())
+    unsorted.append(unsorted_t)
+print('\n'.join(sorted(unsorted,key=int)))
