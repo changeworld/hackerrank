20099d4329602d89a37fa6efdf75f31384db8183
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-04 17:13:36 +0900

    Fix Checkstyle warning

--- domains/java/java-introduction/java-date-and-time/Solution.java
+++ domains/java/java-introduction/java-date-and-time/Solution.java
@@ -15,7 +15,9 @@ public class Solution {
     int year = in.nextInt();
     Calendar ca = Calendar.getInstance();
     ca.set(year, month - 1, day);
-    System.out.println(new DateFormatSymbols().getWeekdays()[ca.get(Calendar.DAY_OF_WEEK)].toUpperCase());
+    System.out.println(
+        new DateFormatSymbols().getWeekdays()[ca.get(Calendar.DAY_OF_WEEK)].toUpperCase()
+    );
     in.close();
   }
 }
