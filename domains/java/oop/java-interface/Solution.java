d0e47a1fca46d1e99c9ec26e701be0e28af5b947
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-05 13:20:10 +0900

    Fix Checkstyle warnings

--- domains/java/oop/java-interface/Solution.java
+++ domains/java/oop/java-interface/Solution.java
@@ -7,23 +7,32 @@ interface AdvancedArithmetic {
 class MyCalculator implements AdvancedArithmetic {
   public int divisor_sum(int n) {
     int a = 0;
-    for (int i = 1; i <= n; i++) if (n % i == 0) a += i;
+    for (int i = 1; i <= n; i++) {
+      if (n % i == 0) {
+        a += i;
+      }
+    }
     return a;
   }
 }
 
 class Solution {
+  /**
+   * Java Interface Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
-    MyCalculator my_calculator = new MyCalculator();
+    MyCalculator myCalculator = new MyCalculator();
     System.out.print("I implemented: ");
-    ImplementedInterfaceNames(my_calculator);
+    implementedInterfaceNames(my_calculator);
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     System.out.print(my_calculator.divisor_sum(n) + "\n");
     sc.close();
   }
 
-  static void ImplementedInterfaceNames(Object o) {
+  static void implementedInterfaceNames(Object o) {
     Class[] theInterfaces = o.getClass().getInterfaces();
     for (int i = 0; i < theInterfaces.length; i++) {
       String interfaceName = theInterfaces[i].getName();
