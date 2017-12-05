f35cb5e3f9cf1f949d9f06ed2d7983711927bf1d
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-05 11:14:33 +0900

    Fix Checkstyle warning

--- domains/java/java-introduction/java-loops/Solution.java
+++ domains/java/java-introduction/java-loops/Solution.java
@@ -10,7 +10,9 @@ public class Solution {
     Scanner sc = new Scanner(System.in);
     int t = sc.nextInt();
     for (; t > 0; t--) {
-      int a = sc.nextInt(), b = sc.nextInt(), n = sc.nextInt();
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int n = sc.nextInt();
       for (int i = 0; i < n; i++) {
         System.out.print(a + b * ((2 << i) - 1));
         System.out.print(i < n - 1 ? " " : "\n");
