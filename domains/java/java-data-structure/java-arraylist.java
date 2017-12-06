f63121e41c1bd70d09bde2aa441190a89e84747f
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-06 09:29:34 +0900

    Fix Checkstyle warnings

--- domains/java/java-data-structure/java-arraylist.java
+++ domains/java/java-data-structure/java-arraylist.java
@@ -2,24 +2,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Java Arraylist Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = Integer.parseInt(sc.next());
+    int n = Integer.parseInt(sc.next());
     ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-    for (int n = 0; n < N; n++) {
-      int M = Integer.parseInt(sc.next());
+    for (int i = 0; i < n; i++) {
+      int m = Integer.parseInt(sc.next());
       list.add(new ArrayList<Integer>());
-      for (int m = 0; m < M; m++) {
+      for (int j = 0; j < m; j++) {
         int x = Integer.parseInt(sc.next());
-                ;
         list.get(n).add(x);
       }
     }
-    int Q = Integer.parseInt(sc.next());
-    for (int q = 0; q < Q; q++) {
-      int x = Integer.parseInt(sc.next()), y = Integer.parseInt(sc.next());
-      if (x > list.size() || y > list.get(x - 1).size()) System.out.println("ERROR!");
-      else System.out.println(list.get(x - 1).get(y - 1));
+    int q = Integer.parseInt(sc.next());
+    for (int k = 0; k < q; k++) {
+      int x = Integer.parseInt(sc.next());
+      int y = Integer.parseInt(sc.next());
+      if (x > list.size() || y > list.get(x - 1).size()) {
+        System.out.println("ERROR!");
+      } else {
+        System.out.println(list.get(x - 1).get(y - 1));
+      }
     }
   }
 }
