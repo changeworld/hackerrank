8a4c6ff6c677a485dfd39b6b4b0ceba889e909fd
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-06 14:09:58 +0900

    Fix Checkstyle warnings

--- domains/java/java-strings/java-string-reverse/Solution.java
+++ domains/java/java-strings/java-string-reverse/Solution.java
@@ -1,14 +1,19 @@
 import java.lang.StringBuffer;
 import java.util.Scanner;
 
-public class Solution{
-  public static void main(String[] args){
+public class Solution {
+  /**
+   * Java String Reverse Solution.
+   * @author changeworld
+   *
+   */
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String a = sc.next();
     System.out.println(a.equals(reverse(a)) ? "Yes" : "No");
   }
 
-  static String reverse(String s){
+  static String reverse(String s) {
     return new StringBuffer(s).reverse().toString();
   }
 }
