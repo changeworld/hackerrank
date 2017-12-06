46df7e6fae6dfa15a5980fa70865f5b2eaab7803
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-06 13:47:21 +0900

    Fix Checkstyle warnings

--- domains/java/java-data-structure/java-generics/Solution.java
+++ domains/java/java-data-structure/java-generics/Solution.java
@@ -2,12 +2,19 @@ import java.lang.reflect.Method;
 
 class Printer {
   <T> void printArray(T[] input) {
-    for (T e : input) System.out.println(e);
+    for (T e : input) {
+      System.out.println(e);
+    }
   }
 }
 
 public class Solution {
-  public static void main(String args[]) {
+  /**
+   * Java Generics Solution.
+   * @author changeworld
+   *
+   */
+  public static void main(String[] args) {
     Printer myPrinter = new Printer();
     Integer[] intArray = {1, 2, 3};
     String[] stringArray = {"Hello", "World"};
@@ -16,10 +23,12 @@ public class Solution {
     int count = 0;
     for (Method method : Printer.class.getDeclaredMethods()) {
       String name = method.getName();
-
-      if (name.equals("printArray"))
-                count++;
+      if (name.equals("printArray")) {
+        count++;
+      }
     }
-    if (count > 1) System.out.println("Method overloading is not allowed!");
+    if (count > 1) {
+      System.out.println("Method overloading is not allowed!");
+    }
   }
 }
