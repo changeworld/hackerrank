133f82d8838b3101413519ba1c0a957136d394d3
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-10 22:46:57 +0900

    Fix Reek warnings

--- domains/algorithms/warmup/birthday-cake-candles.rb
+++ domains/algorithms/warmup/birthday-cake-candles.rb
@@ -1,10 +1,5 @@
 #!/bin/ruby
 
-def birthdayCakeCandles(n, ar)
-  ar.count(ar.max)
-end
-
 n = gets.strip.to_i
 ar = gets.strip.split.map(&:to_i)
-result = birthdayCakeCandles(n, ar)
-puts result;
+put ar.count(ar.max)
