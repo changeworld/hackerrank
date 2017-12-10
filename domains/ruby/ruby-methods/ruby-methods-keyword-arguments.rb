c69a918d2c0206f4a0807ac745f031374527667d
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-10 21:59:44 +0900

    Fix Reek warnings

--- domains/ruby/ruby-methods/ruby-methods-keyword-arguments.rb
+++ domains/ruby/ruby-methods/ruby-methods-keyword-arguments.rb
@@ -1,9 +1,12 @@
-# Your code here
-def convert_temp(n, option)
+#!/bin/ruby
+
+# :reek:UtilityFunction:public_methods_only: true
+# :reek:TooManyStatements { max_statements: 6 }
+def convert_temp(num, option)
   celsius = case option[:input_scale]
-    when 'kelvin' then n - 273.15
-    when 'fahrenheit' then (n - 32) * 5 / 9.0
-    else n
+    when 'kelvin' then num - 273.15
+    when 'fahrenheit' then (num - 32) * 5 / 9.0
+    else num
   end
 
   temp = case option[:output_scale]
