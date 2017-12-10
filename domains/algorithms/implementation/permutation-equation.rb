e3391cfe1e9f6409d937e4fd3ede43a72b2c118e
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-10 10:12:47 +0900

    Fix Reek warnings

--- domains/algorithms/implementation/permutation-equation.rb
+++ domains/algorithms/implementation/permutation-equation.rb
@@ -1,7 +1,8 @@
 #!/bin/ruby
 
-def func_p(n)
-  Sequence_p[n - 1]
+# :reek:UtilityFunction:public_methods_only: true
+def func_p(num)
+  Sequence_p[num - 1]
 end
 
 n = gets.strip.to_i
@@ -10,4 +11,4 @@ a = gets.strip.split.map(&:to_i)
 Sequence_p = []
 a.each_with_index{|x, i| Sequence_p[x-1] = i+1}
 
-(1..n).each{|i| p func_p(func_p(i)) }
+(1..n).each{|i| p func_p(func_p(i))}
