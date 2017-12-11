fd927a02b910f01dc8b5ae4321d6d05f0191d73a
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 12:16:43 +0900

    Fix Reek warnings

--- domains/algorithms/implementation/breaking-best-and-worst-records.rb
+++ domains/algorithms/implementation/breaking-best-and-worst-records.rb
@@ -1,24 +1,17 @@
 #!/bin/ruby
 
-def getRecord(s)
-  cnt_max = 0
-  cnt_min = 0
-  max = s[0]
-  min = s[0]
-  s.length.times do |i|
-    if (s[i] > max)
-      max = s[i]
-      cnt_max = cnt_max + 1
-    end
-    if (s[i] < min)
-      min = s[i]
-      cnt_min = cnt_min + 1
-    end
-  end
-  [cnt_max, cnt_min]
-end
-
 n = gets.strip.to_i
 s = gets.strip.split.map(&:to_i)
-result = getRecord(s)
-print result.join(" ")
+cnt_max, cnt_min = 0, 0
+max, min = s[0], s[0]
+s.length.times {|i|
+  if (s[i] > max)
+    max = s[i]
+    cnt_max = cnt_max + 1
+  end
+  if (s[i] < min)
+    min = s[i]
+    cnt_min = cnt_min + 1
+  end
+}
+puts [cnt_max, cnt_min].join(" ")
