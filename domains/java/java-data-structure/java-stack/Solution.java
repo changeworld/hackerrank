b393324c0f00bd89c697dd02daa5549727f9516d
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 12:04:42 +0900

    Fix Checkstyle warnings

--- domains/java/java-data-structure/java-stack/Solution.java
+++ domains/java/java-data-structure/java-stack/Solution.java
@@ -9,18 +9,21 @@ class Solution {
   static boolean decision(String str) {
     Stack<Character> stack = new Stack<>();
     for (int i = 0; i < str.length(); i++) {
-      if (parentheses.containsKey(str.charAt(i))) stack.push(str.charAt(i));
-      else if (stack.empty() || parentheses.get(stack.pop()) != str.charAt(i)) return false;
+      if (parentheses.containsKey(str.charAt(i))) {
+        stack.push(str.charAt(i));
+      } else if (stack.empty() || parentheses.get(stack.pop()) != str.charAt(i)) {
+        return false;
+      }
     }
     return stack.empty();
   }
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
     parentheses = new HashMap<>();
     parentheses.put('(', ')');
     parentheses.put('{', '}');
     parentheses.put('[', ']');
+    Scanner sc = new Scanner(System.in);
     while (sc.hasNext()) {
       System.out.println(decision(sc.nextLine()));
     }
