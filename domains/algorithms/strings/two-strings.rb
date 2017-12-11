ea25ffd5742de26e2526b00fdc6e93e871114650
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 11:07:07 +0900

    Fix Reek warnings

--- domains/algorithms/strings/two-strings.rb
+++ domains/algorithms/strings/two-strings.rb
@@ -1,14 +1,10 @@
 #!/bin/ruby
 require 'set'
 
-def twoStrings(s1, s2)
-  x = Set.new(s1.split(''))
-  y = Set.new(s2.split(''))
-  (x & y).empty? ? 'NO' : 'YES'
-end
-
 gets.strip.to_i.times{
   s1 = gets.strip
   s2 = gets.strip
-  puts twoStrings(s1, s2)
+  x = Set.new(s1.split(''))
+  y = Set.new(s2.split(''))
+  puts (x & y).empty? ? 'NO' : 'YES'
 }
