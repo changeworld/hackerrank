ba66235fc0500852332c79fc3641fbf512e7e36b
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 09:48:46 +0900

    Fix Checkstyle warning

--- domains/java/java-strings/java-string-compare/Solution.java
+++ domains/java/java-strings/java-string-compare/Solution.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Java Substring Comparisons Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String str = sc.next();
