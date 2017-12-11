2bf46572e62d19f71e51273c3d1677c23a545a8f
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 11:44:03 +0900

    Fix Checkstyle warnings

--- domains/java/java-introduction/java-currency-formatter/Solution.java
+++ domains/java/java-introduction/java-currency-formatter/Solution.java
@@ -12,9 +12,17 @@ public class Solution {
     Scanner scanner = new Scanner(System.in);
     double payment = scanner.nextDouble();
     scanner.close();
-    System.out.println("US: " + NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(payment));
-    System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment));
-    System.out.println("China: " + NumberFormat.getCurrencyInstance(new Locale("zh", "CN")).format(payment));
-    System.out.println("France: " + NumberFormat.getCurrencyInstance(new Locale("fr", "FR")).format(payment));
+    System.out.println(
+        "US: " + NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(payment)
+    );
+    System.out.println(
+        "India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment)
+    );
+    System.out.println(
+        "China: " + NumberFormat.getCurrencyInstance(new Locale("zh", "CN")).format(payment)
+    );
+    System.out.println(
+        "France: " + NumberFormat.getCurrencyInstance(new Locale("fr", "FR")).format(payment)
+    );
   }
 }
