9e264eccefe09b37d657fadb2e512c98cc972c0b
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 09:58:08 +0900

    Fix Checkstyle warnings

--- domains/java/oop/java-abstract-class.java
+++ domains/java/oop/java-abstract-class.java
@@ -17,12 +17,17 @@ class MyBook extends Book {
 }
 
 public class Main {
+  /**
+   * Java Abstract Class Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String title = sc.nextLine();
-    MyBook new_novel = new MyBook();
-    new_novel.setTitle(title);
-    System.out.println("The title is: " + new_novel.getTitle());
+    MyBook novel = new MyBook();
+    novel.setTitle(title);
+    System.out.println("The title is: " + novel.getTitle());
     sc.close();
   }
 }
