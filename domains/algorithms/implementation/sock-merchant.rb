f5b08a86b92d20adb3e2e1011c8a6d85d41e075d
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-11 14:11:42 +0900

    Fix Reek warnings

--- domains/algorithms/implementation/sock-merchant.rb
+++ domains/algorithms/implementation/sock-merchant.rb
@@ -1,12 +1,7 @@
 #!/bin/ruby
 
-def sockMerchant(n, ar)
-  num = 0
-  ar.inject(Hash.new(0)){|k, i| k[i] += 1; k}.each_value{|f| num += f/2}
-  num
-end
-
 n = gets.strip.to_i
 ar = gets.strip.split.map(&:to_i)
-result = sockMerchant(n, ar)
-puts result;
+num = 0
+ar.inject(Hash.new(0)){|k, i| k[i] += 1; k}.each_value{|f| num += f/2}
+puts num
