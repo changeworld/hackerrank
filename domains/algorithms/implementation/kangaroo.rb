0b23eb923d1ec562e74e6f7e5e4b2996455a0097
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-12 17:00:36 +0900

    Fix Reek warnings

--- domains/algorithms/implementation/kangaroo.rb
+++ domains/algorithms/implementation/kangaroo.rb
@@ -1,13 +1,8 @@
 #!/bin/ruby
 
-def kangaroo(x1, v1, x2, v2)
-  if (x1 > x2 and v1 >= v2) or (x1 < x2 and v1 <= v2) or (x1-x2).abs % (v1-v2).abs != 0
-    'NO'
-  else
-    'YES'
-  end
-end
-
 x1, v1, x2, v2 = gets.strip.split.map(&:to_i)
-result = kangaroo(x1, v1, x2, v2)
-puts result
+if (x1 > x2 and v1 >= v2) or (x1 < x2 and v1 <= v2) or (x1-x2).abs % (v1-v2).abs != 0
+  puts 'NO'
+else
+  puts 'YES'
+end
