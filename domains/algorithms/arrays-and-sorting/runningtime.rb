164204f206b30d82749812c326b101658c0e0f5f
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-12 15:31:01 +0900

    Fix Reek warnings

--- domains/algorithms/arrays-and-sorting/runningtime.rb
+++ domains/algorithms/arrays-and-sorting/runningtime.rb
@@ -1,21 +1,18 @@
 #!/bin/ruby
-def insertion_sort(arr)
-  count = 0
-  (1..(arr.length-1)).each do |i|
-    index = i
-    (i-1).downto(0) do |j|
-      if arr[j] > arr[index]
-        arr[j], arr[index] = arr[index], arr[j]
-        index = j
-        count += 1
-      else
-        break
-      end
-    end
-  end
-  count
-end
 
 _ = gets
 arr = gets.chomp.split.map(&:to_i)
-puts insertion_sort(arr)
+count = 0
+(1..(arr.length-1)).each do |i|
+  index = i
+  (i-1).downto(0) do |j|
+    if arr[j] > arr[index]
+      arr[j], arr[index] = arr[index], arr[j]
+      index = j
+      count += 1
+    else
+      break
+    end
+  end
+end
+p count
