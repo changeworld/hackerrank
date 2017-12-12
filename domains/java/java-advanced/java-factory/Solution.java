31f1b44ecb0ca5c6341af34f4979452dbd839d2b
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-12 09:10:30 +0900

    Fix Checkstyle warning

--- domains/java/java-advanced/java-factory/Solution.java
+++ domains/java/java-advanced/java-factory/Solution.java
@@ -36,20 +36,20 @@ public class Solution {
    *
    */
   public static void main(String[] args) {
-    Do_Not_Terminate.forbidExit();
+    DoNotTerminate.forbidExit();
     try {
       Scanner sc = new Scanner(System.in);
       FoodFactory foodFactory = new FoodFactory();
       Food food = foodFactory.getFood(sc.nextLine());
       System.out.println("The factory returned " + food.getClass());
       System.out.println(food.getType());
-    } catch (Do_Not_Terminate.ExitTrappedException e) {
+    } catch (DoNotTerminate.ExitTrappedException e) {
       System.out.println("Unsuccessful Termination!!");
     }
   }
 }
 
-class Do_Not_Terminate {
+class DoNotTerminate {
   public static class ExitTrappedException extends SecurityException {
     private static final long serialVersionUID = 1L;
   }
