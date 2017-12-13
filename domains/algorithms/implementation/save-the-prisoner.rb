fd226bcff4a926406f6f38b0eecc82f706ede44a
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-13 10:29:38 +0900

    Fix Reek warnings

--- domains/algorithms/implementation/save-the-prisoner.rb
+++ domains/algorithms/implementation/save-the-prisoner.rb
@@ -1,12 +1,6 @@
 #!/bin/ruby
 
-def saveThePrisoner(n, m, s)
-  ((m+s-2)%n)+1
-end
-
-t = gets.strip.to_i
-for a0 in (0..t-1)
+gets.strip.to_i.times{
   n, m, s = gets.strip.split.map(&:to_i)
-  result = saveThePrisoner(n, m, s)
-  puts result
-end
+  puts ((m+s-2)%n)+1
+}
