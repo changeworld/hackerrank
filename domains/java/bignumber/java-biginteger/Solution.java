b934d11cdebd0fa43aa23654ea1b5b5d88285a25
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-13 11:07:15 +0900

    Fix Checkstyle warnings

--- domains/java/bignumber/java-biginteger/Solution.java
+++ domains/java/bignumber/java-biginteger/Solution.java
@@ -2,9 +2,15 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Java BigInteger Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    BigInteger a = sc.nextBigInteger(), b = sc.nextBigInteger();
+    BigInteger a = sc.nextBigInteger();
+    BigInteger b = sc.nextBigInteger();
     System.out.println(a.add(b));
     System.out.println(a.multiply(b));
   }
