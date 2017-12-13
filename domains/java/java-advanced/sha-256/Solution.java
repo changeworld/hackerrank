4918cc318545c2f6883c4e63ed47ea5c6a0d076f
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-13 13:26:50 +0900

    Fix Checkstyle warnings

--- domains/java/java-advanced/sha-256/Solution.java
+++ domains/java/java-advanced/sha-256/Solution.java
@@ -2,6 +2,11 @@ import java.security.MessageDigest;
 import java.util.Scanner;
 
 class Solution {
+  /**
+   * Java SHA-256 Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
@@ -9,8 +14,11 @@ class Solution {
       MessageDigest md = MessageDigest.getInstance("SHA-256");
       md.update(str.getBytes());
       byte[] digest = md.digest();
-      for (byte b : digest) System.out.printf("%02x", b);
+      for (byte b : digest) {
+        System.out.printf("%02x", b);
+      }
     } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
