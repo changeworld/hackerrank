e6429d83f6fa82fc30118ac0e67d05218083cfe3
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-13 14:11:43 +0900

    Fix Checkstyle warnings

--- domains/java/java-strings/java-string-tokens/Solution.java
+++ domains/java/java-strings/java-string-tokens/Solution.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Java String Tokens Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String[] text = sc.nextLine().trim().split("[ !,?\\._'@]+", 0);
@@ -8,7 +13,9 @@ public class Solution {
       System.out.println(0);
     } else {
       System.out.println(text.length);
-      for (String s : text) System.out.println(s);
+      for (String s : text) {
+        System.out.println(s);
+      }
     }
   }
 }
