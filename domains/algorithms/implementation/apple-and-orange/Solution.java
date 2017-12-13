7c8ca5f15b112f0fe8af10be862f1a10bf7f911b
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-13 15:41:36 +0900

    Fix Checkstyle warning

--- domains/algorithms/implementation/apple-and-orange/Solution.java
+++ domains/algorithms/implementation/apple-and-orange/Solution.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Apple and Orange Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int s = in.nextInt();
