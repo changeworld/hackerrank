ce69cc6ad8cbc8b968380b72dbfad8f0a81becc3
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-14 15:34:33 +0900

    Fix Reek warnings

--- domains/algorithms/arrays-and-sorting/quicksort2.rb
+++ domains/algorithms/arrays-and-sorting/quicksort2.rb
@@ -1,16 +1,19 @@
 #!/bin/ruby
+
+# :reek:DuplicateMethodCall { max_calls: 2 }
+# :reek:UtilityFunction:public_methods_only: true
 def partition(arr)
-  p = arr.first
-  f = arr[1..-1].partition{|n| n < p}
-  [f[0].size, f[0]+[p]+f[1]]
+  fst = arr.first
+  ar = arr[1..-1].partition{|num| num < fst}
+  [ar[0].size, ar[0]+[fst]+ar[1]]
 end
 
 def quicksort(arr)
   return arr if arr.size < 2
-  n, a = partition(arr)
-  r = quicksort(a[0...n])+[a[n]]+quicksort(a[(n+1)..-1])
-  puts r*' '
-  r
+  num, ar = partition(arr)
+  ret = quicksort(ar[0...num])+[ar[num]]+quicksort(ar[(num+1)..-1])
+  puts ret*' '
+  ret
 end
 
 gets
