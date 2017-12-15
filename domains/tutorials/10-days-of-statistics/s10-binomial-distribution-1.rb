c06c8e0e7466e775f6a6ca7fdaee1e8dcfb9d259
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-15 14:01:16 +0900

    Fix Reek warnings

--- domains/tutorials/10-days-of-statistics/s10-binomial-distribution-1.rb
+++ domains/tutorials/10-days-of-statistics/s10-binomial-distribution-1.rb
@@ -10,16 +10,22 @@ g g g g g g ->  1
 =end
 
 class Integer
-    def comb(r)
-        if r == 0 then return 1 end
-        n = self
-        ret = 1
-        if r > n/2 then r = n-r end
-        r.times{|i|
-            ret = ret*(n-i)/(i+1)
-        }
-        return ret
+  # :reek:FeatureEnvy:exclude
+  # :reek:TooManyStatements { max_statements: 7 }
+  def comb(inp)
+    if inp == 0
+      return 1
     end
+    num = self
+    ret = 1
+    if inp > num/2
+      inp = num-inp
+    end
+    inp.times{|idx|
+      ret = ret*(num-idx)/(idx+1)
+    }
+    return ret
+  end
 end
 
 b = 1.09/2.09
