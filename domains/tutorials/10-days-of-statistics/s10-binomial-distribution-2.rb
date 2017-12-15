d8b798135a46547b6ea0b44a0ab51b3a21a0b8e1
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-15 14:11:39 +0900

    Fix Reek warnings

--- domains/tutorials/10-days-of-statistics/s10-binomial-distribution-2.rb
+++ domains/tutorials/10-days-of-statistics/s10-binomial-distribution-2.rb
@@ -1,16 +1,23 @@
 #!/bin/ruby
 
+# The Integer class extended for Day 4: Binomial Distribution II
 class Integer
-    def comb(r)
-        if r == 0 then return 1 end
-        n = self
-        ret = 1
-        if r > n/2 then r = n-r end
-        r.times{|i|
-            ret = ret*(n-i)/(i+1)
-        }
-        return ret
+  # :reek:FeatureEnvy:exclude
+  # :reek:TooManyStatements { max_statements: 7 }
+  def comb(inp)
+    if inp == 0
+      return 1
     end
+    num = self
+    ret = 1
+    if inp > num/2
+      inp = num-inp
+    end
+    inp.times{|idx|
+      ret = ret*(num-idx)/(idx+1)
+    }
+    return ret
+  end
 end
 
 r = 0.12
