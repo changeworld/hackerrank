311d952566f9b3077e4d88dfb57df18a572ce7ee
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-18 17:09:00 +0900

    Fix Checkstyle warning

--- domains/java/oop/java-interface/Solution.java
+++ domains/java/oop/java-interface/Solution.java
@@ -1,21 +1,5 @@
 import java.util.Scanner;
 
-interface AdvancedArithmetic {
-  int divisor_sum(int n);
-}
-
-class MyCalculator implements AdvancedArithmetic {
-  public int divisor_sum(int n) {
-    int a = 0;
-    for (int i = 1; i <= n; i++) {
-      if (n % i == 0) {
-        a += i;
-      }
-    }
-    return a;
-  }
-}
-
 class Solution {
   /**
    * Java Interface Solution.
@@ -38,5 +22,21 @@ class Solution {
       String interfaceName = theInterfaces[i].getName();
       System.out.println(interfaceName);
     }
+  }
+}
+
+interface AdvancedArithmetic {
+  int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+  public int divisor_sum(int n) {
+    int a = 0;
+    for (int i = 1; i <= n; i++) {
+      if (n % i == 0) {
+        a += i;
+      }
+    }
+    return a;
   }
 }
