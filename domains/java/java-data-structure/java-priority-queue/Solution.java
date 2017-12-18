063539dbb59e2b9e07c895e051f802a871c781d7
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-18 15:03:59 +0900

    Fix Checkstyle warning

--- domains/java/java-data-structure/java-priority-queue/Solution.java
+++ domains/java/java-data-structure/java-priority-queue/Solution.java
@@ -1,6 +1,41 @@
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+class Solution {
+  /**
+   * Java Priority Queue Solution.
+   * @author changeworld
+   *
+   */
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int totalEvents = Integer.parseInt(in.nextLine());
+    PriorityQueue<Student> queue = new PriorityQueue<Student>();
+    for (int i = 0; i < totalEvents; i++) {
+      String event = in.next();
+      if (event.equals("SERVED")) {
+        queue.poll();
+      } else {
+        String name = in.next();
+        double cgpa = in.nextDouble();
+        int id = in.nextInt();
+        queue.offer(new Student(id, name, cgpa));
+      }
+    }
+    if (queue.size() == 0) {
+      System.out.println("EMPTY");
+    } else {
+      for (; ; ) {
+        Student student = queue.poll();
+        if (student == null) {
+          break;
+        }
+        System.out.println(student.getFname());
+      }
+    }
+  }
+}
+
 class Student implements Comparable<Student> {
   private int token;
   private String fname;
@@ -43,40 +78,5 @@ class Student implements Comparable<Student> {
       return 1;
     }
     return 0;
-  }
-}
-
-class Solution {
-  /**
-   * Java Priority Queue Solution.
-   * @author changeworld
-   *
-   */
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int totalEvents = Integer.parseInt(in.nextLine());
-    PriorityQueue<Student> queue = new PriorityQueue<Student>();
-    for (int i = 0; i < totalEvents; i++) {
-      String event = in.next();
-      if (event.equals("SERVED")) {
-        queue.poll();
-      } else {
-        String name = in.next();
-        double cgpa = in.nextDouble();
-        int id = in.nextInt();
-        queue.offer(new Student(id, name, cgpa));
-      }
-    }
-    if (queue.size() == 0) {
-      System.out.println("EMPTY");
-    } else {
-      for (; ; ) {
-        Student student = queue.poll();
-        if (student == null) {
-          break;
-        }
-        System.out.println(student.getFname());
-      }
-    }
   }
 }
