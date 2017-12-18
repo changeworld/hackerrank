bca2cb8060459f3205092f7fef21237748934fc7
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-18 16:26:03 +0900

    Fix Checkstyle warnings

--- domains/algorithms/implementation/acm-icpc-team/Solution.java
+++ domains/algorithms/implementation/acm-icpc-team/Solution.java
@@ -3,9 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
+  /**
+   * ACM ICPC Team Solution.
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String input[] = br.readLine().split(" ");
+    String[] input = br.readLine().split(" ");
     int n = Integer.parseInt(input[0]);
     int m = Integer.parseInt(input[1]);
     byte[][] arrKnowledge = new byte[n][m];
