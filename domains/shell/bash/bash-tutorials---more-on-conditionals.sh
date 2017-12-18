f5812667e68d8ecfde5b406079093539e6ef2b75
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-18 13:59:31 +0900

    Fix ShellCheck notes

--- domains/shell/bash/bash-tutorials---more-on-conditionals.sh
+++ domains/shell/bash/bash-tutorials---more-on-conditionals.sh
@@ -4,7 +4,7 @@ read -r X
 read -r Y
 read -r Z
 
-if [[ $X == $Y && $X == $Z ]]; then
+if [[ X == Y && X == Z ]]; then
     echo "EQUILATERAL"
 elif [[ $X == $Y || $X == $Z || $Y == $Z ]] ; then
     echo "ISOSCELES"
