86fc06d470f395f5123622a96f2d57a136e87784
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-21 12:56:24 +0900

    Fix Checkstyle warnings

---
+++ domains/java/oop/java-inheritance-1/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+  /**
+   * Java Inheritance I Solution.
+   * @author changeworld
+   *
+   */
+  public static void main(String[] args) {
+    Bird bird = new Bird();
+    bird.walk();
+    bird.fly();
+    bird.sing();
+  }
+}
