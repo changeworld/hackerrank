58a74382032e6e385687a2ba89d5b3ba1f8a9427
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-21 14:57:22 +0900

    Fix Checkstyle warnings

--- domains/java/oop/java-interface/Solution.java
+++ domains/java/oop/java-interface/Solution.java
@@ -24,19 +24,3 @@ class Solution {
     }
   }
 }
-
-interface AdvancedArithmetic {
-  int divisor_sum(int n);
-}
-
-class MyCalculator implements AdvancedArithmetic {
-  public int divisor_sum(int n) {
-    int a = 0;
-    for (int i = 1; i <= n; i++) {
-      if (n % i == 0) {
-        a += i;
-      }
-    }
-    return a;
-  }
-}
