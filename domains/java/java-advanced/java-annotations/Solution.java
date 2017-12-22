415a28d00e20943024b5eb37a13203c1bd838cf1
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-22 13:36:40 +0900

    Fix Checkstyle warning

--- domains/java/java-advanced/java-annotations/Solution.java
+++ domains/java/java-advanced/java-annotations/Solution.java
@@ -1,33 +1,5 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 import java.util.Scanner;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@interface FamilyBudget {
-  String userRole() default "GUEST";
-
-  int budgetLimit() default 0;
-}
-
-class FamilyMember {
-  @FamilyBudget(userRole = "SENIOR", budgetLimit = 100)
-  public void seniorMember(int budget, int moneySpend) {
-    System.out.println("Senior Member");
-    System.out.println("Spend: " + moneySpend);
-    System.out.println("Budget Left: " + (budget - moneySpend));
-  }
-
-  @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
-  public void juniorUser(int budget, int moneySpend) {
-    System.out.println("Junior Member");
-    System.out.println("Spend: " + moneySpend);
-    System.out.println("Budget Left: " + (budget - moneySpend));
-  }
-}
 
 public class Solution {
   /**
