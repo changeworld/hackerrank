5c55ecd3499b51e9461fdbfda1a24bf04fd02df1
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-26 00:58:19 +0900

    Sherlock and Array

---
+++ domains/algorithms/search/sherlock-and-array.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+for i in range(int(input())):
+    input()
+    nums = list(map(int, input().split(' ')))
+    sum_l, sum_r, result = 0, sum(nums), 'NO'
+    for num in nums:
+        if (sum_r - num)/2 == sum_l:
+            result = 'YES'
+            break
+        sum_l += num
+    print(result)
