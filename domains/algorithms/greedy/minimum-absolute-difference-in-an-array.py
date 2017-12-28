ded7abcc14ccd45e4b64665c5be72531540749c8
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-28 10:46:13 +0900

    Fix autopep8 errors

--- domains/algorithms/greedy/minimum-absolute-difference-in-an-array.py
+++ domains/algorithms/greedy/minimum-absolute-difference-in-an-array.py
@@ -3,4 +3,4 @@
 if __name__ == "__main__":
     n = input()
     a = sorted(map(int, input().split()))
-    print(min(abs(x-y) for x, y in zip(a, a[1:])))
+    print(min(abs(x - y) for x, y in zip(a, a[1:])))
--- domains/algorithms/search/sherlock-and-array.py
+++ domains/algorithms/search/sherlock-and-array.py
@@ -5,7 +5,7 @@ for i in range(int(input())):
     nums = list(map(int, input().split(' ')))
     sum_l, sum_r, result = 0, sum(nums), 'NO'
     for num in nums:
-        if (sum_r - num)/2 == sum_l:
+        if (sum_r - num) / 2 == sum_l:
             result = 'YES'
             break
         sum_l += num
