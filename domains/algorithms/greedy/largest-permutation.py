96028edce0cf920420bb97b0549743cbc4220eb6
Takashi Takebayashi <changesworlds@gmail.com>
2018-01-09 10:14:16 +0900

    Fix YAPF warnings

--- domains/algorithms/greedy/largest-permutation.py
+++ domains/algorithms/greedy/largest-permutation.py
@@ -5,21 +5,21 @@ l = list(map(int, input().split()))
 ans = []
 for i in range(n):
     if i < k:
-        ans.append(str(n-i))
+        ans.append(str(n - i))
         if l[i] == (n - i):
-            k+=1
+            k += 1
         elif l[i] > (n - i):
             num = l[i]
-            while num > (n-i):
+            while num > (n - i):
                 num = l[n - num]
             if num == (n - i):
-                k+=1
+                k += 1
     else:
         if l[i] <= (n - k):
             ans.append(str(l[i]))
         else:
             num = l[i]
-            while num > (n-k):
+            while num > (n - k):
                 num = l[n - num]
             ans.append(str(num))
 print(" ".join(ans))
--- domains/algorithms/greedy/maximum-perimeter-triangle.py
+++ domains/algorithms/greedy/maximum-perimeter-triangle.py
@@ -3,9 +3,9 @@
 i = int(input()) - 3
 a = sorted(int(i) for i in input().split())
 
-while i >= 0 and (a[i] + a[i+1] <= a[i+2]):
+while i >= 0 and (a[i] + a[i + 1] <= a[i + 2]):
     i -= 1
 if i >= 0:
-    print(a[i], a[i+1], a[i+2])
+    print(a[i], a[i + 1], a[i + 2])
 else:
     print(-1)
