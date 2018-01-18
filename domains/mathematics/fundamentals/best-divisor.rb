98d0b8327aa2ed5870bde0fb83fc65431e24546b
Takashi Takebayashi <changesworlds@gmail.com>
2018-01-18 10:58:05 +0900

    Refactoring

--- domains/mathematics/fundamentals/best-divisor.rb
+++ domains/mathematics/fundamentals/best-divisor.rb
@@ -6,5 +6,5 @@ for i in 2..(n/2)
   d.push(i) if n%i == 0
 end
 d.push(n)
-s = d.map{|x| x.to_s.split('').map(&:to_i).reduce(:+)}
+s = d.map{|x| x.to_s.split.map(&:to_i).reduce(:+)}
 p d[s.index(s.max)]
