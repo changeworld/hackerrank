ad4b3f861e4c9efc63ac92b3402771142c4e5194
Takashi Takebayashi <changesworlds@gmail.com>
2018-01-22 00:35:46 +0900

    Sherlock and Divisors

---
+++ domains/mathematics/fundamentals/sherlock-and-divisors.py
@@ -0,0 +1,5 @@
+import functools
+
+for _ in range(int(input())):
+    n = int(input())
+    print(len(set(filter(lambda x: x%2 == 0, functools.reduce(lambda x,y: x+y, ((i, n/i) for i in range(1, int(n**0.5)+1) if n%i == 0))))))
