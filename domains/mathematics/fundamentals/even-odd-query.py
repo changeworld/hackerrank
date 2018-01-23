04dd00d0776c871b4d32b450f1141b2162a4e267
Takashi Takebayashi <changesworlds@gmail.com>
2018-01-24 00:32:37 +0900

    Even Odd Query

---
+++ domains/mathematics/fundamentals/even-odd-query.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+n = int(input())
+l = [int(x) for x in input().split()]
+for _ in range(int(input())):
+    x,y = [int(i) for i in input().split()]
+    if l[x-1] % 2 != 0:
+        print("Odd")
+    else:
+        if x == n:
+            print("Even")
+        else:
+            if l[x] == 0 and (y-x) > 0:
+                print("Odd")
+            else:
+                print("Even")
