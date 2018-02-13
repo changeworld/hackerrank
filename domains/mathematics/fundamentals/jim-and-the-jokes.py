8d5241c8106119e4b5dbcba9b94472b2072bd6e9
Takashi Takebayashi <changesworlds@gmail.com>
2018-02-14 00:29:15 +0900

    Jim and the Jokes

---
+++ domains/mathematics/fundamentals/jim-and-the-jokes.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import collections
+
+a = []
+for _ in range(int(input())):
+    m,d = [int(temp) for temp in input().split()]
+    if d // 10 >= m or d % 10 >= m:
+        pass
+    else:
+        a.append((d//10)*m+d%10)
+ans = 0
+for i in collections.Counter(a).items():
+    ans += i[1]*(i[1]-1)//2
+print(ans)
