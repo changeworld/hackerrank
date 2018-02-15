0909363009138e22d0b14a5c61b31b0f37d206bb
Takashi Takebayashi <changesworlds@gmail.com>
2018-02-15 16:30:09 +0900

    Fix autopep8 error

--- domains/mathematics/fundamentals/jim-and-the-jokes.py
+++ domains/mathematics/fundamentals/jim-and-the-jokes.py
@@ -3,12 +3,12 @@ import collections
 
 a = []
 for _ in range(int(input())):
-    m,d = [int(temp) for temp in input().split()]
+    m, d = [int(temp) for temp in input().split()]
     if d // 10 >= m or d % 10 >= m:
         pass
     else:
-        a.append((d//10)*m+d%10)
+        a.append((d // 10) * m + d % 10)
 ans = 0
 for i in collections.Counter(a).items():
-    ans += i[1]*(i[1]-1)//2
+    ans += i[1] * (i[1] - 1) // 2
 print(ans)
