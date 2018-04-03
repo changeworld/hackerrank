e0da1448dea9cc1cafefc68715420c76cd988d77
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-04 00:39:18 +0900

    Euler's Criterion

---
+++ domains/mathematics/number-theory/eulers-criterion.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+for _ in range(int(input())):
+    A,M = (map(int, input().split()))
+    if A == 0:
+        print('YES')
+    elif pow(A,(M-1)//2,M) == 1:
+        print('YES')
+    else:
+        print('NO')
