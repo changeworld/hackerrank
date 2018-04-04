d0525e34d2ba41d43c75dd7f066cb26b2a8fb656
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-04 15:59:21 +0900

    Fix YAPF errors

--- domains/mathematics/number-theory/eulers-criterion.py
+++ domains/mathematics/number-theory/eulers-criterion.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
 for _ in range(int(input())):
-    A,M = (map(int, input().split()))
+    A, M = (map(int, input().split()))
     if A == 0:
         print('YES')
-    elif pow(A,(M-1)//2,M) == 1:
+    elif pow(A, (M - 1) // 2, M) == 1:
         print('YES')
     else:
         print('NO')
