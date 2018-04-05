002306c8b6602e696485253e81a5b51de0a48ede
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-06 00:56:24 +0900

    Picking Cards

---
+++ domains/mathematics/combinatorics/picking-cards.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+def calc(cards):
+    l = len(cards)
+    r = 1
+    for i, c in enumerate(cards):
+        v = l - c
+        if v <= 0:
+            return 0
+        r *= (v - i)
+    return r % 1000000007
+
+for _ in range(int(input())):
+    _ = input()
+    cards = list(map(int, input().strip().split()))
+    cards.sort(reverse=True)
+    print(calc(cards))
