3b2b4e7048fcb970dca5a69717f45ce99ff97e42
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-13 00:44:18 +0900

    Power of large numbers

---
+++ domains/mathematics/number-theory/power-of-large-numbers.rb
@@ -0,0 +1,17 @@
+#!/bin/ruby
+
+MOD=1000000007
+def base_exponent_mod(x, y)
+  z = 1
+  while y != 0
+    z = z*x%MOD if y&1 != 0
+    x = x*x%MOD
+    y >>= 1
+  end
+  z
+end
+
+gets.to_i.times{
+    x,y = gets.split.map(&:to_i)
+    p base_exponent_mod(x%MOD, y%~-MOD)
+}
