324866500700194e6e70bb82ecdb110bdf3c136a
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-19 00:23:41 +0900

    Merge List

---
+++ domains/mathematics/combinatorics/merge-list.rb
@@ -0,0 +1,19 @@
+#!/bin/ruby
+
+class Integer
+  def comb(x)
+    return 1 if x == 0
+    n = self
+    ret = 1
+    x = n-x if x > n/2
+    x.times{|i|
+      ret = ret*(n-i)/(i+1)
+    }
+    ret
+  end
+end
+
+gets.to_i.times{
+  x, y = gets.split.map(&:to_i)
+  p (x+y).comb(x)%(10**9+7)
+}
