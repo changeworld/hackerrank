1f3c2a7bfb06eda22f715966f69701144f1a3089
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-19 10:48:57 +0900

    Fix autopep8 errors

--- domains/mathematics/fundamentals/russian-peasant-exponentiation.py
+++ domains/mathematics/fundamentals/russian-peasant-exponentiation.py
@@ -1,19 +1,21 @@
 #!/bin/python3
 
-def square(ar,ai,br,bi,m):
-    return ((ar*br)%m - (ai*bi)%m)%m, ((ar*bi)%m + (ai*br)%m)%m 
 
-def calc(a,b,p,m):
+def square(ar, ai, br, bi, m):
+    return ((ar * br) % m - (ai * bi) % m) % m, ((ar * bi) % m + (ai * br) % m) % m
+
+
+def calc(a, b, p, m):
     resr = 1
     resi = 0
     while p > 0:
-        if p%2 == 1:
-            resr,resi = square(resr,resi,a,b,m)
-        a,b = square(a,b,a,b,m)
+        if p % 2 == 1:
+            resr, resi = square(resr, resi, a, b, m)
+        a, b = square(a, b, a, b, m)
         p = p//2
-    return resr,resi
+    return resr, resi
 
 for _ in range(int(input())):
-    a,b,k,m = map(int, input().split(' '))
-    ansr,ansi = calc(a,b,k,m)
-    print('{0} {1}'.format(ansr,ansi))
+    a, b, k, m = map(int, input().split(' '))
+    ansr, ansi = calc(a, b, k, m)
+    print('{0} {1}'.format(ansr, ansi))
--- domains/mathematics/fundamentals/sherlock-and-divisors.py
+++ domains/mathematics/fundamentals/sherlock-and-divisors.py
@@ -1,5 +1,5 @@
-import functools
+from functools import reduce
 
 for _ in range(int(input())):
     n = int(input())
-    print(len(set(filter(lambda x: x%2 == 0, functools.reduce(lambda x,y: x+y, ((i, n/i) for i in range(1, int(n**0.5)+1) if n%i == 0))))))
+    print(len(set(filter(lambda x: x % 2 == 0, reduce(lambda x, y: x + y, ((i, n / i) for i in range(1, int(n**0.5) + 1) if n % i == 0))))))
