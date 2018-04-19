cf534a321832240dfb0a58ce0444da44e147dd38
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-19 09:56:15 +0900

    Fix Reek:TooManyStatements

--- domains/mathematics/combinatorics/merge-list.rb
+++ domains/mathematics/combinatorics/merge-list.rb
@@ -1,17 +1,15 @@
 #!/bin/ruby
 
 # :reek:FeatureEnvy:exclude
+# :reek:TooManyStatements {max_statements: 6}
 # :reek:UncommunicativeParameterName
 # :reek:UncommunicativeVariableName
 class Integer
   def comb(x)
     return 1 if x == 0
-    n = self
-    ret = 1
+    n = self, ret = 1
     x = n-x if x > n/2
-    x.times{|i|
-      ret = ret*(n-i)/(i+1)
-    }
+    x.times{|i| ret = ret*(n-i)/(i+1)}
     ret
   end
 end
