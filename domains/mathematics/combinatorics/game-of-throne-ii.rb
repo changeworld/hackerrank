e9b65fc2a41ccac8092731937cee8677e6e01101
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-22 00:55:43 +0900

    Game Of Thrones - II

---
+++ domains/mathematics/combinatorics/game-of-throne-ii.rb
@@ -0,0 +1,16 @@
+#!/bin/ruby
+
+def perm(n, k)
+  k.times.reduce(1){|s,i| s *= n-i}
+end
+
+h = Hash.new(0)
+gets.chomp.each_char{|c| h[c]+=1}
+a = h.map{|k, v| v/2}
+l = a.reduce(:+)
+s = 1
+a.each{|e|
+  s = s * perm(l, e) / perm(e, e)
+  l -= e
+}
+p s % 1000000007
