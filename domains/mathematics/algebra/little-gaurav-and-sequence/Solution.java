ac53f56f981fde81bb94575780509feb7de9ed91
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-27 09:54:09 +0900

    Fix Checkstyle warnings

--- domains/mathematics/algebra/little-gaurav-and-sequence/Solution.java
+++ domains/mathematics/algebra/little-gaurav-and-sequence/Solution.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 
 public class Solution {
+  /**
+   * Little Gaurav and Sequence
+   * @author changeworld
+   *
+   */
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int t = in.nextInt();
-    for (int i = 0; i < t; i++){
+    for (int i = 0; i < t; i++) {
       long n = in.nextLong();
-      double d = Math.log(n)/Math.log(2);
-      int j = (int)d+1;
+      double d = Math.log(n) / Math.log(2);
+      int j = (int)d + 1;
       int s = 0;
-      if (j == 1)
+      if (j == 1) {
         s = 2;
-      else
-        s = (j-1)*6;
-      int x = s%10;
+      } else {
+        s = (j - 1) * 6;
+      }
+      int x = s % 10;
       int y = 5;
-      if (n%2 == 0)
+      if (n % 2 == 0) {
         y = 1;
-      int z = (x*y)%10;
+      }
+      int z = (x * y) % 10;
       System.out.println(z);
     }
   }
