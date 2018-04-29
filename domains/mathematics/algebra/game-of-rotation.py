4889e03f813bdbe8455b24f862626316947279aa
Takashi Takebayashi <changesworlds@gmail.com>
2018-04-30 00:37:19 +0900

    Game Of Rotation

---
+++ domains/mathematics/algebra/game-of-rotation.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = [int(i) for i in input().split()]
+o = pmean = sum(i * j for i, j in enumerate(a, 1))
+s = sum(a)
+for i in a:
+    pmean += n * i - s
+    o = max(o, pmean)
+print(o)
