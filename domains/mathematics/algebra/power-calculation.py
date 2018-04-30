16317af0161c24e3fa61cd815df643caae7e9b5a
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-01 00:47:51 +0900

    Power Calculation

---
+++ domains/mathematics/algebra/power-calculation.py
@@ -0,0 +1,6 @@
+from itertools import accumulate
+
+for _ in range(int(input())):
+    k, n = list(map(int, input().split()))
+    x = list(accumulate(pow(i, n, 100) for i in range(100)))
+    print('%02d' % (((k // 100) * x[-1] + x[k % 100]) % 100))
