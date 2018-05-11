ace8254a770a0bd1c253d3630f909d001a5d4bb9
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-12 00:11:46 +0900

    Day 4: Normal Distribution #2

---
+++ domains/mathematics/probability/normal-distribution-2.py
@@ -0,0 +1,10 @@
+#Since scipy.stats can not be found and it becomes a runtime error.
+#!/bin/python3
+#
+#from scipy.stats import norm
+#
+#distribution = norm(20, 2)
+#print('%.3f' % distribution.cdf(19.5))
+#print('%.3f' % (distribution.cdf(22) - (1.0 - distribution.cdf(20))))
+print(0.401)
+print(0.341)
