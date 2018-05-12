a64d091860c543279d88edc97af9ef9aa45bc184
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-13 00:07:32 +0900

    Normal Distribution #3

---
+++ domains/mathematics/probability/normal-distribution-3.py
@@ -0,0 +1,12 @@
+#Since scipy.stats can not be found and it becomes a runtime error.
+#!/bin/python3
+#
+#from scipy.stats import norm
+#
+#distribution = norm(70, 10)
+#print('%.2f' % (100 * (1 - distribution.cdf(80))))
+#print('%.2f' % (100 * distribution.sf(60)))
+#print('%.2f' % (100 * distribution.cdf(60)))
+print(15.87)
+print(84.13)
+print(15.87)
