e4aed770280599682c49211692673aeda79d797b
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-17 10:35:38 +0900

    Fix YAPF warning

--- domains/mathematics/probability/binomial-distribution-1.py
+++ domains/mathematics/probability/binomial-distribution-1.py
@@ -1,9 +1,13 @@
 #!/bin/python2
 
+
 def comb(n, r):
     x = 1
-    for i in range(r): x = x * (n - i) // (i + 1)
+    for i in range(r):
+        x = x * (n - i) // (i + 1)
     return x
+
+
 n = 4
 p = 0.8
 q = 1 - p
