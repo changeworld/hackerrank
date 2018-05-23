78fb9455ab1f00a8de990ec598f195bda1bcae81
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-23 13:09:19 +0900

    Fix YAPF warning

--- domains/mathematics/algebra/shashank-and-list.py
+++ domains/mathematics/algebra/shashank-and-list.py
@@ -3,4 +3,6 @@
 from functools import reduce
 mod = 10**9 + 7
 _ = input()
-print(reduce(lambda a, b: a * b % mod, [1 + pow(2, int(x), mod) for x in input().split()], 1) - 1)
+a = lambda a, b: a * b % mod
+b = [1 + pow(2, int(x), mod) for x in input().split()]
+print(reduce(a, b, 1) - 1)
