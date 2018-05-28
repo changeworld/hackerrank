b5f0bf01f76272406f27fbc6f2abb389ad24ac30
Takashi Takebayashi <changesworlds@gmail.com>
2018-05-28 10:23:50 +0900

    Fix autopep8 warning

--- domains/mathematics/fundamentals/lowest-triangle.py
+++ domains/mathematics/fundamentals/lowest-triangle.py
@@ -1,6 +1,10 @@
 #!/bin/python3
 import math
 
-x = lambda x: math.ceil(2 * x[1] / x[0])
+
+def x(x):
+    return math.ceil(2 * x[1] / x[0])
+
+
 y = [[int(i) for i in input().strip().split(' ')]]
 print(int(list(map(x, y))[0]))
