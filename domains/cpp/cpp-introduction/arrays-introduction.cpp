60d9d03dc4e4f4702ca05ddbbf48219dbe413ae1
Takashi Takebayashi <changesworlds@gmail.com>
2018-06-12 00:25:14 +0900

    Arrays Introduction

---
+++ domains/cpp/cpp-introduction/arrays-introduction.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    vector<int>v(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[n - i - 1]);
+    }
+    for (int i = 0; i < n; i++) {
+        printf(i < n - 1? "%d " : "%d\n", v[i]);
+    }
+    return 0;
+}
